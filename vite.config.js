fddf608474dacf057b7550902dd7ec6b7192191c
spitkov <github@spitkov.hu>
2025-08-10 16:55:42 +0200

    vite not svelte

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()],
+  preview: {
+    allowedHosts: ["localhost", "127.0.0.1", "ipa.firka.app"],
+  },
 });
